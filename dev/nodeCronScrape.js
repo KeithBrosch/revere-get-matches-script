8205a9bceb02a8afc25d955c0354edf0019cd40b
mark craven <mpcrav01@gmail.com>
2022-02-05 11:45:27 -0500

    nodeCronScrape file created

--- dev/nodeCronScrape.js
+++ dev/nodeCronScrape.js
@@ -1,6 +1,6 @@
-const valorant = require('../utils/valorant-scrape');
-const cron = require('node-cron');
-console.log("starting cron");
+// const valorant = require('../utils/valorant-scrape');
+// const cron = require('node-cron');
+// console.log("starting cron");
 // cron.schedule('0 */5 * * * *', () => {
 //   valorant.getMatchesStartingSoon();
 // });
