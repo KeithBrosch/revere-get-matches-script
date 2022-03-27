90ff79d92fe062b8dfc79283c74db0cc1df32570
mark craven <mpcrav01@gmail.com>
2022-03-27 12:24:51 -0400

    add 20sec dealy to match scrape call after the scheduler kicks off the script. meant to offset potential scheduler inaccuarcy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const valorant = require("./utils/valorant-scrape")
-
+const scrapeDelayBuffer = (20000) //wait 20 seconds after scheduler calls script in case scheduler fires early
 console.log(`Starting heroku scheduler scrape at ${new Date()}`);
-valorant.getMatchesStartingSoon(false);
+setTimeout(() => valorant.getMatchesStartingSoon(false), scrapeDelayBuffer);
 
